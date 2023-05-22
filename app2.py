aa917ec6d18a85ccf51c8a4856b72310cf8b3e73
bradcastro <134321688+bradcastro@users.noreply.github.com>
2023-05-22 15:18:55 -0500

    Update and rename app.py to app2.py

---
+++ app2.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+# List of renewable energy stocks
+renewable_stocks = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'FB']  # Add more renewable energy stocks here
+
+st.title('Renewable Energy Stock Recommendations')
+
+# Download historical data for renewable energy stocks
+data = yf.download(renewable_stocks, start='2020-01-01', end='2023-12-31')
+
+# Create an empty DataFrame to hold the recommendation data
+recommendations = pd.DataFrame(columns=['Stock', 'Recommendation'])
+
+# Perform analysis and generate recommendations
+for stock in renewable_stocks:
+    # Perform your analysis here to generate stock recommendations
+    # You can use various metrics and analysis techniques to evaluate the stocks
+    
+    # For demonstration purposes, let's assume a simple random recommendation
+    recommendation = 'Buy' if st.session_state[stock] else 'Sell'
+    
+    recommendations = recommendations.append({'Stock': stock, 'Recommendation': recommendation}, ignore_index=True)
+
+# Display the stock recommendations
+st.subheader('Stock Recommendations')
+st.dataframe(recommendations)
+
+# Allow users to customize their preferences
+st.subheader('Customize Preferences')
+
+for stock in renewable_stocks:
+    st.sidebar.markdown(f"**{stock}**")
+    st.sidebar.checkbox('Buy', key=stock)
