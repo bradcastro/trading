f235db7f68a29e7ebdb4182135e509cf0cd2ac23
bradcastro <134321688+bradcastro@users.noreply.github.com>
2023-05-22 16:21:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,34 +11,37 @@ def generate_recommendations():
               'CRWD', 'DECK', 'DNOW', 'DRQ', 'FLR']
 
     for stock in stocks:
-        data = yf.download(stock, start=(datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d"),
-                           end=datetime.now().strftime("%Y-%m-%d"), progress=False)
+        try:
+            data = yf.download(stock, start=(datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d"),
+                               end=datetime.now().strftime("%Y-%m-%d"), progress=False)
 
-        if data.empty:
-            continue
+            if data.empty:
+                continue
 
-        # Calculate buy and sell prices based on the previous day's close price
-        buy_price = data['Close'].iloc[-2]
-        sell_price = data['Close'].iloc[-1]
+            # Calculate buy and sell prices based on the previous day's close price
+            buy_price = data['Close'].iloc[-2]
+            sell_price = data['Close'].iloc[-1]
 
-        # Perform your recommendation logic here
-        recommendation = 'Buy' if sell_price > buy_price else 'Sell'
+            # Perform your recommendation logic here
+            recommendation = 'Buy' if sell_price > buy_price else 'Sell'
 
-        recommendations.append({'Stock': stock, 'Buy Price': buy_price,
-                                'Sell Price': sell_price, 'Recommendation': recommendation})
+            recommendations.append({'Stock': stock, 'Buy Price': buy_price,
+                                    'Sell Price': sell_price, 'Recommendation': recommendation})
+        except Exception as e:
+            print(f"Error occurred for stock {stock}: {str(e)}")
 
     recommendations_df = pd.DataFrame(recommendations)
-    recommendations_df['Stock'] = recommendations_df['Stock'].apply(lambda x: f"[{x}](https://finance.yahoo.com/quote/{x})")
+    recommendations_df['Stock'] = recommendations_df['Stock'].apply(
+        lambda x: f"[{x}](https://finance.yahoo.com/quote/{x})")
 
     return recommendations_df
 
 
 # Main app code
-st.title("Bagwells Big Bag Recommendations")
+st.title("Bagwells Big Bag")
 recommendations = generate_recommendations()
 
 if recommendations.empty:
     st.info("No recommendations available.")
 else:
-    recommendations['Stock'] = recommendations['Stock'].apply(lambda x: f"[{x}](https://finance.yahoo.com/quote/{x})")
     st.table(recommendations)
