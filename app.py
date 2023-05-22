d8cdaa67df1a2d021bff14499f0ab455113b06f0
bradcastro <134321688+bradcastro@users.noreply.github.com>
2023-05-22 15:46:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,25 +18,28 @@ def generate_recommendations():
 
     # Iterate through each stock ticker
     for stock in tickers:
-        # Download historical data for the stock
-        data = yf.download(stock, start=current_date, end=end_date, progress=False)
-        
-        if data.empty:
-            continue
-        
-        # Calculate the purchase price as the closing price of the current day
-        purchase_price = data['Close'][current_date.strftime('%Y-%m-%d')]
+        try:
+            # Download historical data for the stock
+            data = yf.download(stock, start=current_date, end=end_date, progress=False)
+            
+            if data.empty:
+                continue
+            
+            # Calculate the purchase price as the closing price of the current day
+            purchase_price = data['Close'][current_date.strftime('%Y-%m-%d')]
 
-        # Calculate the sell price as the mean of the high and low prices of the current day
-        sell_price = (data['High'][current_date.strftime('%Y-%m-%d')] + data['Low'][current_date.strftime('%Y-%m-%d')]) / 2
+            # Calculate the sell price as the mean of the high and low prices of the current day
+            sell_price = (data['High'][current_date.strftime('%Y-%m-%d')] + data['Low'][current_date.strftime('%Y-%m-%d')]) / 2
 
-        # Determine the recommendation based on the purchase and sell prices
-        if sell_price > purchase_price:
-            recommendation = 'Buy'
-        else:
-            recommendation = 'Sell'
+            # Determine the recommendation based on the purchase and sell prices
+            if sell_price > purchase_price:
+                recommendation = 'Buy'
+            else:
+                recommendation = 'Sell'
 
-        recommendations.append({'Stock': stock, 'Recommendation': recommendation, 'Purchase Price': purchase_price, 'Sell Price': sell_price})
+            recommendations.append({'Stock': stock, 'Recommendation': recommendation, 'Purchase Price': purchase_price, 'Sell Price': sell_price})
+        except Exception as e:
+            print(f"Failed to generate recommendation for {stock}: {e}")
 
     return recommendations
 
@@ -49,6 +52,11 @@ st.header("Bagwells Big Bag - Stock Recommendations for the Next Week")
 
 if recommendations:
     df = pd.DataFrame(recommendations)
+    
+    # Format the dataframe to include hyperlinks
+    df['Stock'] = df['Stock'].apply(lambda x: f"[{x}](https://finance.yahoo.com/quote/{x})")
+    
+    # Display the dataframe
     st.dataframe(df.style.format({'Purchase Price': '{:.2f}', 'Sell Price': '{:.2f}'}), height=800)
 else:
     st.write("No recommendations available")
