f05c9745efb792c3bbc4d8093cc0c09289e8632c
bradcastro <134321688+bradcastro@users.noreply.github.com>
2023-05-22 15:44:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
+from datetime import datetime, timedelta
 
 # List of stock tickers
 tickers = ['RES', 'SCHW', 'SLB', 'SPR', 'STRL', 'SWBI', 'THO', 'TPR', 'NOV', 'OBTC', 'OII', 'OIS', 'ONEW', 'ORN', 'POWL', 'PVH', 'FLR', 'FOSL', 'GBX', 'HOOD', 'JWN', 'KBAL', 'MOV', 'MRMD', 'MTRX', 'BKE', 'CLB', 'CNK', 'CRWD', 'DECK', 'DNOW', 'DRQ', 'FLR']
 
-# Function to generate future recommendations based on trailing 7-day performance
+# Function to generate future recommendations for the next week
 def generate_recommendations():
     recommendations = []
+
+    # Get the current date
+    current_date = datetime.now()
+
+    # Calculate the end date as one week from the current date
+    end_date = current_date + timedelta(days=7)
 
     # Iterate through each stock ticker
     for stock in tickers:
         # Download historical data for the stock
-        data = yf.download(stock, period='7d', interval='1d', progress=False)
+        data = yf.download(stock, start=current_date, end=end_date, progress=False)
         
         if data.empty:
             continue
         
-        # Calculate the purchase price as the closing price of the last trading day
-        purchase_price = data['Close'][-1]
+        # Calculate the purchase price as the closing price of the current day
+        purchase_price = data['Close'][current_date.strftime('%Y-%m-%d')]
 
-        # Calculate the sell price as the mean of the high and low prices of the last trading day
-        sell_price = (data['High'][-1] + data['Low'][-1]) / 2
+        # Calculate the sell price as the mean of the high and low prices of the current day
+        sell_price = (data['High'][current_date.strftime('%Y-%m-%d')] + data['Low'][current_date.strftime('%Y-%m-%d')]) / 2
 
         # Determine the recommendation based on the purchase and sell prices
         if sell_price > purchase_price:
@@ -38,7 +45,7 @@ recommendations = generate_recommendations()
 
 # Display recommendations in Streamlit app
 st.set_page_config(layout="wide")
-st.header("Bagwells Big Bag - Stock Recommendations for the Next Trading Day")
+st.header("Bagwells Big Bag - Stock Recommendations for the Next Week")
 
 if recommendations:
     df = pd.DataFrame(recommendations)
